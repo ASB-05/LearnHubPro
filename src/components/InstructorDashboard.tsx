8aff490128df6c08f56eca0f8608047295353b56
ShenoyAsh <shenoybashwini@gmail.com>
2025-11-11 18:14:42 +0530

    updated code

--- src/components/InstructorDashboard.tsx
+++ src/components/InstructorDashboard.tsx
@@ -1,4 +1,4 @@
-import { User } from '../App'; // <-- ADD THIS
+import { User } from '../App'; 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
 import { Button } from './ui/button';
 import { Badge } from './ui/badge';
